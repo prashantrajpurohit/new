d8d18d845d00c8dd4501a9b10f91dd9bcb69b356
Prashant <dev.prashant.rajpurohit@gmail.com>
2022-11-20 22:12:05 +0530

    first commit

---
+++ client/src/Navbar.jsx
@@ -0,0 +1,16 @@
+export function Navbar(){
+return (
+    <div className="main">
+    <section >
+        <a href="#">BATTERY-WALA</a>
+    </section>
+    <div className="nav">
+ <li><a className="active" href="#">HOME </a></li>
+ <li><a href="#">CONTACT</a></li>
+ <li><a href="#">ABOUT</a></li>
+ <li><a href="#">LOGIN</a></li>
+    </div>
+    </div>
+)
+
+}
