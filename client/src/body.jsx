a5cd5b39626e0b8a9adeeed5fb72bbb77bfeed6d
Prashant <dev.prashant.rajpurohit@gmail.com>
2022-11-24 23:35:42 +0530

    body changes

--- client/src/body.jsx
+++ client/src/body.jsx
@@ -5,7 +5,7 @@ export function Body(){
         <h2>Find the Perfect</h2>
         <h1>Battery within Seconds</h1>
         <p>KNOW MORE ABOUT YOUR BATTERY</p>
-            <button>SHOP NOW</button>
+           <a href="#"><button>SHOP NOW</button></a> 
         </section>
         </>
     )
